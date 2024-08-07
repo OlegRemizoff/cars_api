a197039e01c9276dca91bb6e627344ae116a9a16
oleg_remizov <job.rem1zoff@gmail.com>
2024-08-07 12:51:59 +0300

    add retrive method for AutoViewSet

--- app/cars/views.py
+++ app/cars/views.py
@@ -1,10 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
+
 from cars.serializers import AutoSerializer
 from cars.filters import PriceRangeFilter
 from cars.models import Auto
 
-# ?max_price=450000
 
 class AutoViewSet(ModelViewSet):
     queryset = Auto.objects.all()
@@ -12,7 +14,8 @@ class AutoViewSet(ModelViewSet):
     filter_backends = [DjangoFilterBackend, PriceRangeFilter]
     filterset_fields = ["model", "fuel", "transmission", "name", "year",  "mileage", "price"]
 
-
-
-
-
+    def retrieve(self, request, pk=None):
+        queryset = Auto.objects.all()
+        car = get_object_or_404(queryset, pk=pk)
+        serializer = AutoSerializer(car)
+        return Response(serializer.data)
