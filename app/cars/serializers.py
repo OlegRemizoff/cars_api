cf96386254a40da9b28b10a110ad11f4ab0aad83
oleg_remizov <job.rem1zoff@gmail.com>
2024-08-14 17:38:21 +0300

    corrected fields in serializer

--- app/cars/serializers.py
+++ app/cars/serializers.py
@@ -1,9 +1,13 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from cars.models import Auto
 
 
 class AutoSerializer(ModelSerializer):
+    model = serializers.ReadOnlyField(source='model.name')
+    fuel = serializers.CharField(source='get_fuel_display')
+    transmission = serializers.CharField(source='get_transmission_display')
     
     class Meta:
         model = Auto
-        fields = "__all__"
+        fields = ["id", "name", "model", "year", "model", "fuel", "transmission", "price"]
